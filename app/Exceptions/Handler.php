eec8284fc7dfbca35dc36663840a778d62e92f23
safari <sshsfl@yeah.net>
2015-12-24 12:07:36 +0800

    add exception handler for the ApiException and OAuthException

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,10 @@
 
 namespace App\Exceptions;
 
+use Lang;
 use Exception;
+use Illuminate\Http\JsonResponse;
+use League\OAuth2\Server\Exception\OAuthException;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
@@ -46,6 +49,61 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $e)
     {
+        if ($e instanceof ApiException) {
+            return $this->customApiExceptionHandler($e);
+        }
+
+        if ($e instanceof OAuthException) {
+            return $this->customOAuthExceptionHandler($e);
+        }
+
         return parent::render($request, $e);
     }
+
+    private function customApiExceptionHandler($e)
+    {
+        $res = [
+            'error' => $e->errorType,
+            'error_description' => $e->getMessage()
+        ];
+
+        if ($code = $e->getCode()) {
+            $res['error_code'] = $code;
+        }
+        if ($uri = $e->errorUri) {
+            $res['error_uri'] = $uri;
+        }
+
+        return new JsonResponse($res, $e->httpStatusCode, $e->getHttpHeaders());
+    }
+
+    private function customOAuthExceptionHandler($e)
+    {
+        $attrs = [];
+
+        $errorType    = $e->errorType;
+        $errorMessage = $e->getMessage();
+
+        if ($errorType == 'invalid_request') {
+            if ($errorMessage == 'The refresh token is invalid.') {
+                $errorType = 'invalid_refresh_token';
+            } else {
+                preg_match('/"(.*?)"/', $errorMessage, $matches);
+                $parameter = $matches[1];
+                $attrs['parameter'] = $parameter;
+            }
+        }
+
+        $message = Lang::get('oauth.'.$errorType, $attrs);
+        $message = starts_with($message, 'oauth.') ? $errorMessage : $message;
+
+        return new JsonResponse([
+                'error' => $e->errorType,
+                'error_description' => $message,
+            ],
+            $e->httpStatusCode,
+            $e->getHttpHeaders()
+        );
+    }
+
 }
