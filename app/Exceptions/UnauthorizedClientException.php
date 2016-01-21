5d71121bec745aaac769d0629ef07d42cc4b9724
safari <sshsfl@yeah.net>
2016-01-21 15:15:14 +0800

    add exception file

---
+++ app/Exceptions/UnauthorizedClientException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Exceptions;
+
+use Lang;
+
+class UnauthorizedClientException extends ApiException
+{
+    public function __construct()
+    {
+        parent::__construct(Lang::get('oauth.unauthorized_client'), 14003);
+        $this->httpStatusCode = 401;
+        $this->errorType = 'unauthorized_client';
+    }
+}
