7c9453aae9091dec5cf540d89d05a1fb84980565
safarishi <sshsfl@yeah.net>
2016-01-27 21:45:38 +0800

    Update CommonController
    增加 获取用户id方法

--- app/Http/Controllers/CommonController.php
+++ app/Http/Controllers/CommonController.php
@@ -4,6 +4,11 @@ namespace App\Http\Controllers;
 
 class CommonController extends ApiController
 {
+    /**
+     * 根据 Access Token 获取用户 ID
+     *
+     * @return string
+     */
     protected function getOwnerId()
     {
         $this->authorizer->validateAccessToken();
